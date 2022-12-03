4696592ea28fda9ccf7901dfabcec4191bcccfb7
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2022-12-02 20:04:38 -0800

    done 278

--- algo.js
+++ algo.js
@@ -141,3 +141,42 @@
 //         }
 //     }
 // };
+// ------------------------------------------------------
+// Leet Code 278. First Bad Version
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function (n) {
+        let start = 1;
+        let end = n;
+        let mid;
+        while (start <= end) {
+            mid = Math.floor((start + end) / 2);
+            if (isBadVersion(mid)) {
+                if (isBadVersion(mid - 1)) {
+                    end = mid - 1
+                }
+                else return mid;
+            }
+            else start = mid + 1;
+        }
+
+
+    };
+};
