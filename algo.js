98c044c9464aa86bddb36032f7c89eb69be8bb27
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2022-12-05 09:46:48 -0800

    solved

--- algo.js
+++ algo.js
@@ -238,7 +238,26 @@
 // };
 // Solution without converting integer
 var isPalindrome = function (x) {
-
+    if (x >= 0 && x < 10) return true;
+    if (x < 0) return false;
+    let tempX = x;
+    let revNum = 0;
+    let k = 1;
+    let c = 1;
+    while (x > k) {
+        c++;
+        k *= 10;
+    }
+    let holder = 0;
+    for (let i = 0; i < c; i++) {
+        holder = tempX % 10;
+        tempX = Math.floor(tempX / 10);
+        revNum += holder * k;
+        k /= 10;
+    }
+    if (x - (revNum / 10) == 0) return true;
+    return false;
 };
-const x = 11;
+const x = 121;
 console.log(isPalindrome(x));
+// console.log(x, revNum / 10);
