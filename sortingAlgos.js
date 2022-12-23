9f103610700484fa7498af6e724d3ef06544be07
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2022-12-22 23:35:11 -0500

    s

--- sortingAlgos.js
+++ sortingAlgos.js
@@ -52,27 +52,35 @@
 //
 // ---------------------------------------------------------
 //
-// Quick Sort
-function quickSort (array) {
-    if (array.length === 0) {
-        return [];
-    } else {
-        const pivotValue = array[0];
-        // Sort elements into three piles
-        let lesser = [];
-        let equal = [];
-        let greater = [];
-        for (let e of array) {
-            if (e < pivotValue) {
-                lesser.push(e);
-            } else if (e > pivotValue) {
-                greater.push(e);
-            } else {
-                equal.push(e);
-            }
-        }
-        return [...quickSort(lesser), ...equal, ...quickSort(greater)];
-    }
+// // Quick Sort
+// function quickSort (array) {
+//     if (array.length === 0) {
+//         return [];
+//     } else {
+//         const pivotValue = array[0];
+//         // Sort elements into three piles
+//         let lesser = [];
+//         let equal = [];
+//         let greater = [];
+//         for (let e of array) {
+//             if (e < pivotValue) {
+//                 lesser.push(e);
+//             } else if (e > pivotValue) {
+//                 greater.push(e);
+//             } else {
+//                 equal.push(e);
+//             }
+//         }
+//         return [...quickSort(lesser), ...equal, ...quickSort(greater)];
+//     }
+// }
+//
+//
+// ---------------------------------------------------------
+//
+// Merge Sort
+function mergeSort (array) {
+    // Only change code below this line
+    return array;
+    // Only change code above this line
 }
-const items = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
-console.log(quickSort(items));
