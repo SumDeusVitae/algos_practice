0c02487489d0d0326a47f32922fb7351ab440cd5
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2022-12-22 19:11:40 -0500

    quick sort

--- sortingAlgos.js
+++ sortingAlgos.js
@@ -52,3 +52,27 @@
 //
 // ---------------------------------------------------------
 //
+// Quick Sort
+function quickSort (array) {
+    if (array.length === 0) {
+        return [];
+    } else {
+        const pivotValue = array[0];
+        // Sort elements into three piles
+        let lesser = [];
+        let equal = [];
+        let greater = [];
+        for (let e of array) {
+            if (e < pivotValue) {
+                lesser.push(e);
+            } else if (e > pivotValue) {
+                greater.push(e);
+            } else {
+                equal.push(e);
+            }
+        }
+        return [...quickSort(lesser), ...equal, ...quickSort(greater)];
+    }
+}
+const items = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+console.log(quickSort(items));
