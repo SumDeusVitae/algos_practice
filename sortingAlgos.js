88ca13f5c50287ebec7231f82693710fd8274a6e
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2023-01-02 10:05:45 -0500

    quickSoret Recursive

--- sortingAlgos.js
+++ sortingAlgos.js
@@ -55,16 +55,42 @@
 // // Quick Sort NEED REPEAT AND DO DIFFERENTLY. DEF mby few more
 // Quick sort with single array as entrance
 function quickSort (array) {
-    const pivotValue = array[length];
-    let pivotIndex = 0;
-    for (let i = pivotIndex; i < pivotValue; i++) {
-        if (array[i] < pivotValue) {
-            [array[i], array[pivotIndex]] = [array[pivotIndex], array[i]];
+    if (array.len < 2) {
+        return array;
+    }
+    return quickSortRecursive(array, 0, array.length - 1);
+}
+
+function partition (arr, start, end) {
+    // Taking the last element as the pivot
+    const pivotValue = arr[end];
+    let pivotIndex = start;
+    for (let i = start; i < end; i++) {
+        if (arr[i] < pivotValue) {
+            // Swapping elements
+            [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+            // Moving to next element
             pivotIndex++;
         }
     }
-    // Thatmwas pivot, need to rethink it
 
+    // Putting the pivot value in the middle
+    [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]]
+    return pivotIndex;
+};
+
+function quickSortRecursive (arr, start, end) {
+    // Base case or terminating case
+    if (start >= end) {
+        return;
+    }
+
+    // Returns pivotIndex
+    let index = partition(arr, start, end);
+
+    // Recursively apply the same logic to the left and right subarrays
+    quickSortRecursive(arr, start, index - 1);
+    quickSortRecursive(arr, index + 1, end);
 }
 
 
@@ -114,4 +140,8 @@ function quickSort (array) {
 //     return [...merged, ...ar1, ...ar2];
 // }
 const x = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
-console.log(mergeSort(x));
+quickSort(x)
+let array = [7, -2, 4, 1, 6, 5, 0, -4, 2]
+quickSortRecursive(array, 0, array.length - 1)
+console.log(array)
+console.log(x);
