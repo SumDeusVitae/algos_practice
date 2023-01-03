b1b32b6b3bf276f9d7287656168cf9b89b7299b0
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2023-01-03 18:55:01 -0500

    526

--- sortingAlgos.js
+++ sortingAlgos.js
@@ -54,44 +54,44 @@
 //
 // // Quick Sort NEED REPEAT AND DO DIFFERENTLY. DEF mby few more
 // Quick sort with single array as entrance
-function quickSort (array) {
-    if (array.len < 2) {
-        return array;
-    }
-    return quickSortRecursive(array, 0, array.length - 1);
-}
+// function quickSort (array) {
+//     if (array.len < 2) {
+//         return array;
+//     }
+//     return quickSortRecursive(array, 0, array.length - 1);
+// }
 
-function partition (arr, start, end) {
-    // Taking the last element as the pivot
-    const pivotValue = arr[end];
-    let pivotIndex = start;
-    for (let i = start; i < end; i++) {
-        if (arr[i] < pivotValue) {
-            // Swapping elements
-            [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
-            // Moving to next element
-            pivotIndex++;
-        }
-    }
+// function partition (arr, start, end) {
+//     // Taking the last element as the pivot
+//     const pivotValue = arr[end];
+//     let pivotIndex = start;
+//     for (let i = start; i < end; i++) {
+//         if (arr[i] < pivotValue) {
+//             // Swapping elements
+//             [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+//             // Moving to next element
+//             pivotIndex++;
+//         }
+//     }
 
-    // Putting the pivot value in the middle
-    [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]]
-    return pivotIndex;
-};
+//     // Putting the pivot value in the middle
+//     [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]]
+//     return pivotIndex;
+// };
 
-function quickSortRecursive (arr, start, end) {
-    // Base case or terminating case
-    if (start >= end) {
-        return;
-    }
+// function quickSortRecursive (arr, start, end) {
+//     // Base case or terminating case
+//     if (start >= end) {
+//         return;
+//     }
 
-    // Returns pivotIndex
-    let index = partition(arr, start, end);
+//     // Returns pivotIndex
+//     let index = partition(arr, start, end);
 
-    // Recursively apply the same logic to the left and right subarrays
-    quickSortRecursive(arr, start, index - 1);
-    quickSortRecursive(arr, index + 1, end);
-}
+//     // Recursively apply the same logic to the left and right subarrays
+//     quickSortRecursive(arr, start, index - 1);
+//     quickSortRecursive(arr, index + 1, end);
+// }
 
 
 // function quickSort (array) {
