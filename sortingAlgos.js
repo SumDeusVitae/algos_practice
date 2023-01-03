80a52dcf74c2c90917f09153ce50c30ea7f8d90c
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2023-01-03 18:54:34 -0500

    525

--- sortingAlgos.js
+++ sortingAlgos.js
@@ -139,9 +139,3 @@ function quickSortRecursive (arr, start, end) {
 //     }
 //     return [...merged, ...ar1, ...ar2];
 // }
-const x = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
-quickSort(x)
-let array = [7, -2, 4, 1, 6, 5, 0, -4, 2]
-quickSortRecursive(array, 0, array.length - 1)
-console.log(array)
-console.log(x);
