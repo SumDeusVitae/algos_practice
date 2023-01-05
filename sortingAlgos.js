0f32dc364750d02246361f80ce54b29601172418
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2023-01-05 18:03:33 -0500

    529

--- sortingAlgos.js
+++ sortingAlgos.js
@@ -141,3 +141,53 @@
 // }
 
 // Heap Sort
+
+class MaxHeap {
+    constructor() {
+        this.heap = [];
+    }
+    parentIndex (index) {
+        return Math.floor((index - 1) / 2);
+    }
+    leftChildIndex (index) {
+        return (2 * index + 1);
+    }
+    rightChildIndex (index) {
+        return (2 * index + 2);
+    }
+
+    swap (a, b) {
+        const temp = this.heap[a];
+        this.heap[a] = this.heap[b];
+        this.heap[b] = temp;
+    }
+    insert (item) {
+        this.heap.push(item);
+        let index = this.heap.length - 1,
+            parent = this.parentIndex(index);
+        while (this.heap[parent] && this.heap[parent] < this.heap[index]) {
+            this.swap(parent, index);
+            index = this.parentIndex(index);
+            parent = this.parentIndex(index);
+        }
+    }
+    delete () {
+        let item = this.heap.shift();
+        this.heap.unshift(this.heap.pop());
+        let index = 0;
+        let leftChild = this.leftChildIndex(index);
+        let rightChild = this.rightChildIndex(index);
+        while (this.heap[leftChild] && this.heap[leftChild] > this.heap[index] || this.heap[rightChild] > this.heap[index]) {
+            let max = leftChild;
+            if (this.heap[rightChild] && this.heap[rightChild] > this.heap[max]) {
+                max = rightChild;
+            }
+            rhis.swap(max, index);
+            index = max;
+            leftChild = this.leftChildIndex(max);
+            rightChild = this.rightChildIndex(max);
+        }
+        return item;
+    }
+
+}
