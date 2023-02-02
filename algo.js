477c83137a4ccdd006d93948adeef26319df78b9
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2023-02-02 11:15:46 -0500

    poh

--- algo.js
+++ algo.js
@@ -725,4 +725,29 @@
 //
 // ------------------------------------------------------
 // Leet Code 16. 3Sum Closest(Medium)
-
+var threeSumClosest = function (nums, target) {
+    nums.sort((x, y) => x - y);
+    let closest = Infinity;
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        let left = i + 1, right = nums.length - 1;
+        while (left < right) {
+            const total = nums[i] + nums[left] + nums[right];
+            if (total === target) return target;
+            closest = Math.abs(target - closest) < Math.abs(target - total) ? closest : total;
+            if (total < target) {
+                left++;
+                while (left < right && nums[i] === nums[i - 1]) left++;
+            } else {
+                right--;
+                while (left < right && nums[right] === nums[right + 1]) right--;
+            }
+        }
+    }
+    return closest;
+};
+const nums =
+    [-1, 2, 1, -4, 6];
+const target =
+    1;
+console.log(threeSumClosest(nums, target));
