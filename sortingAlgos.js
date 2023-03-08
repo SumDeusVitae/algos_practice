d43cb8e7b5fcbe6898123f0fcaa1830904efb9e5
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2023-03-08 17:13:02 -0500

    bubble sort

--- sortingAlgos.js
+++ sortingAlgos.js
@@ -1,1 +1,15 @@
 'use strict'
+//Bubble Sort
+const bubbleSort = function (arr) {
+    for (let i = 0; i < arr.length; i++) {
+
+        for (let j = 0; j < arr.length - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
